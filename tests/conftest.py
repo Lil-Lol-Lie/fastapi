c5ddd8d182d13b046a03c88ddc2f13935f4ec9dd
Phuong <snow.067@hotmail.com>
2024-08-21 22:26:37 +1000

    added database

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,3 +81,29 @@ def test_posts(test_user, session, test_user2):
 
     posts = session.query(models.Post).all()
     return posts
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "love@gmail.com",
+                 "password": "123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "love@gmail.com",
+                 "password": "123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
